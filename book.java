ef4ff511ea5f07c37c8967a420b7ef615aa9a00f
tanishachauhan <tanishachauhan3885@gmail.com>
2019-07-20 10:26:05 +0530

    created book.java

---
+++ book.java
@@ -0,0 +1,13 @@
+public class Book
+
+private int bookId;
+private String title;
+private String author;
+private float price;
+
+public void setBookId(int bookId){
+this.bookId= boookId;
+}
+public int getBookId(){
+return boookId;
+}
